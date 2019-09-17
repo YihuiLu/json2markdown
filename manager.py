d53feb69ae2b46f8acfa974f87b97a6aabe02704
yifeilu <yifeilu@citydnatech.com>
2019-09-17 14:43:58 +0800

    修改入口文件

---
+++ manager.py
@@ -0,0 +1,11 @@
+# @Time    : 2019-09-17 14:42
+# @Author  : Teacher Lu
+# @Site    : www.yifeilu.cn
+# @File    : manager.py
+# @Software: PyCharm
+
+from toMD import app
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",
+            port=7777)
